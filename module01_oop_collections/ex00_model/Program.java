210879065f67e640f6549bc4a01f1221504644c5
ghiew <204108785+ghiew-beep@users.noreply.github.com>
2026-03-07 17:13:38 +0800

    added gitignore

---
+++ module01_oop_collections/ex00_model/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+	public static void main(String[] args) {
+		User p1 = new User("John", 1000);
+		User p2 = new User("Kate", 100);
+
+		System.out.println(p1.getName() + "'s balance is: " + p1.getBalance());
+		System.out.println(p2.getName() + "'s balance is: " + p2.getBalance());
+	}
+}
---
+++ module01_oop_collections/ex00_model/User.java
@@ -0,0 +1,38 @@
+public class User {
+	private Integer identifier;
+	private String name;
+	private Integer balance;
+	private Transaction[] transactionHistory;
+	private int transactionCount;
+
+	private static final int MAX_RECORD = 1000;
+
+	public User () {
+		this.identifier = 0;
+		this.name = "Alpha";
+		this.balance = 0;
+		this.transactionHistory = new Transaction[MAX_RECORD];
+		this.transactionCount = 0;
+	}
+
+	public User (String name, Integer balance) {
+		this.identifier = 0;
+		this.name = name;
+		this.balance = balance;
+		this.transactionHistory = new Transaction[MAX_RECORD];
+		this.transactionCount = 0;
+	}
+
+	public Integer getIdentifier() { return identifier; }
+	public String getName() { return name; }
+	public Integer getBalance() { return balance; }
+	public void setBalance(Integer offset) { balance = balance + offset; }
+
+	public Transaction getTransactionHistory(int idx) {
+		if (idx < 0 || idx >= transactionCount || transactionHistory[idx] == null) {
+			System.out.println("Invalid transaction index detected");
+			return null;
+		}
+		return transactionHistory[idx];
+	}
+}
