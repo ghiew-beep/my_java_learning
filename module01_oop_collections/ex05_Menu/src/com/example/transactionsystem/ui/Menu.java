c1c0819c0969e26cd6cf1e93955b4b9144607503
ghiew <204108785+ghiew-beep@users.noreply.github.com>
2026-03-23 16:53:32 +0800

    feat(m01/ex05): add readInt() and processUserChoice() to Menu

--- module01_oop_collections/ex05_Menu/src/com/example/transactionsystem/ui/Menu.java
+++ module01_oop_collections/ex05_Menu/src/com/example/transactionsystem/ui/Menu.java
@@ -1,11 +1,18 @@
 package com.example.transactionsystem.ui;
 
+import com.example.transactionsystem.service.TransactionsService;
+
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Menu {
 	//--[class fields]----------------------------------------------------------
-	String profile;
-	Boolean	devMode = false;
+	private TransactionsService service;
+
+	private boolean	devMode = false;
+	private boolean	showMenu = true;
+
+	private Scanner scanner = new Scanner(System.in);
 
 	//--[constructor]-----------------------------------------------------------
 	private Menu() {}
@@ -13,20 +20,25 @@ public class Menu {
 	public Menu(String profile) {
 		Objects.requireNonNull(profile, "Profile cannot be null");
 
-		if (profile.equals("dev") || profile.equals("production")) {
-			this.profile = profile;
-			if (profile.equals("dev")) {
-				this.devMode = true;
-			}
-		} else {
+		if (!profile.equals("dev") && !profile.equals("production")) {
 			throw new IllegalArgumentException("Only accept profile: dev or production");
 		}
+		this.devMode = profile.equals("dev");
+		service = TransactionsService.getInstance();
 	}
 
 	//--[methods]---------------------------------------------------------------
 	public void start() {
 		while (true) {
+			if (showMenu) {
+				printMenu();
+				showMenu = false;
+			}
 
+			System.out.print("->");
+			int choice = readInt();
+
+			processUserChoice(choice);
 		}
 	}
 
@@ -49,7 +61,41 @@ public class Menu {
 				""";
 		if (devMode) {
 			System.out.println(menuDev);
-		} else
+		} else {
 			System.out.println(menuProduction);
+		}
+	}
+
+	private int readInt() {
+		while (true) {
+			try {
+				return Integer.parseInt(scanner.nextLine().trim());
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid input, please enter a number");
+			}
+		}
+	}
+
+	private void processUserChoice(int choice) {
+		if ((!devMode && (choice < 0 || choice > 5))
+				|| devMode && (choice < 0 || choice > 7)){
+			System.out.println("Invalid choice, refer the menu");
+			return ;
+		}
+		switch (choice) {
+			case 1: promptAddUser(); break;
+			case 2: promptViewUserBalance(); break;
+			case 3: promptPerformTransfer(); break;
+			case 4: promptViewUserTransactionHistory(); break;
+			case 5:
+				if (devMode)
+					promptRemoveTransaction();
+				else
+					System.exit(0);
+				break;
+			case 6: service.getFailedTransactionList(); break;
+			case 7: System.exit(0); break;
+		}
+		showMenu = true;
 	}
 }
